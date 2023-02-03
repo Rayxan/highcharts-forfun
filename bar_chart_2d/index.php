534bba60ca636a4f4bce40f540ee0e054980326e
Raylan <raylanwork@gmail.com>
2023-02-03 02:41:36 -0300

    Arranging the code to simplify future changes

--- bar_chart_2d/index.php
+++ bar_chart_2d/index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,9 +8,10 @@
   <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
   <title>Gerencial</title>
 </head>
+
 <body>
 
-<?php 
+  <?php
   // $con = new mysqli('localhost','root','','db_chart');
   // $query = $con->query("
   //   SELECT custo_individual as custo FROM bar_chart
@@ -22,95 +24,102 @@
 
   //   // var_dump($custo);
   //   // var_dump(json_encode($custo));
-?>
+  ?>
 
-<?php
-$json = file_get_contents('dados.json');
-$datas = json_decode($json, true);
+  <?php
+  $json = file_get_contents('dados.json');
+  $datas = json_decode($json, true);
 
-foreach($datas as $data) {
-  $dados[] = $data;
-}
+  foreach ($datas as $data) {
+    $dados[] = $data;
+  }
 
-$custo=[];
-$custo[] = [
-            $dados[0]['janeiro'],
-            $dados[0]['fevereiro'], 
-            $dados[0]['marco'], 
-            $dados[0]['abril'], 
-            $dados[0]['maio'], 
-            $dados[0]['junho'], 
-            $dados[0]['julho'], 
-            $dados[0]['agosto'], 
-            $dados[0]['setembro'], 
-            $dados[0]['outubro'], 
-            $dados[0]['novembro'], 
-            $dados[0]['dezembro']
-          ];
+  $custo = [];
+  $custo[] = [
+    $dados[0]['janeiro'],
+    $dados[0]['fevereiro'],
+    $dados[0]['marco'],
+    $dados[0]['abril'],
+    $dados[0]['maio'],
+    $dados[0]['junho'],
+    $dados[0]['julho'],
+    $dados[0]['agosto'],
+    $dados[0]['setembro'],
+    $dados[0]['outubro'],
+    $dados[0]['novembro'],
+    $dados[0]['dezembro']
+  ];
 
-// var_dump($cust[0]);
+  // var_dump($cust[0]);
 
-?>
+  ?>
 
-<div style="display: inline-block; width: 500px; margin-right: 85px;">
-  <canvas id="myChart"></canvas>
-</div>
+  <div style="display: inline-block; margin-top: 25px;">
+    <div style="width: 500px; margin-right: 85px;">
+      <label for=""><b>f:t-> I</b></label>
+      <canvas id="myChart"></canvas>
+    </div>
+  </div>
 
-<div style="display: inline-block; width: 500px;">
-  <canvas id="Chart"></canvas>
-</div>
+  <div style="display: inline-block;">
+    <div style="width: 500px;">
+      <label for=""><b>f:t-> I</b></label>
+      <canvas id="Chart"></canvas>
+    </div>
+  </div>
 
-<script>
-   const labels = ["janeiro","fevereiro","março","abril","maio","junho","julho","agosto","setembro","outubro","novembro","dezembro"];
-   const data = {
-    labels: labels,
-    datasets: [{
-      label: 'SECRETARIA MUNICIPAL DE EDUCAÇÃO',
-      data: <?php echo json_encode($custo[0]) ?>,
-      backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-      ],
-      borderColor: [
-        'rgb(255, 99, 132)',
-      ],
-      borderWidth: 1
-    },
-    {
-      label: 'CRECHE LAR MARCIA',
-      data: <?php echo json_encode($custo[0]) ?>,
-      backgroundColor: [
-        'rgba(75, 192, 192, 0.2)',
-      ],
-      borderColor: [
-        'rgb(75, 192, 192)',
-      ],
-      borderWidth: 1
-    }]
-  };
-
-  const config = {
-    type: 'bar',
-    data: data,
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true
+  <script>
+    const labels = ["janeiro", "fevereiro", "março", "abril", "maio", "junho", "julho", "agosto", "setembro", "outubro", "novembro", "dezembro"];
+    const data = {
+      labels: labels,
+      datasets: [{
+          label: 'SECRETARIA MUNICIPAL DE EDUCAÇÃO',
+          data: <?php echo json_encode($custo[0]) ?>,
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+          ],
+          borderColor: [
+            'rgb(255, 99, 132)',
+          ],
+          borderWidth: 1
+        },
+        {
+          label: 'CRECHE LAR MARCIA',
+          data: <?php echo json_encode($custo[0]) ?>,
+          backgroundColor: [
+            'rgba(75, 192, 192, 0.2)',
+          ],
+          borderColor: [
+            'rgb(75, 192, 192)',
+          ],
+          borderWidth: 1
         }
-      }
-    },
-  };
+      ]
+    };
 
-  var myChart = new Chart(
-    document.getElementById('myChart'),
-    config
-  );
+    const config = {
+      type: 'bar',
+      data: data,
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      },
+    };
 
-  var myChart = new Chart(
-    document.getElementById('Chart'),
-    config
-  );
-  
-</script>
+    var myChart = new Chart(
+      document.getElementById('myChart'),
+      config
+    );
+
+    var myChart = new Chart(
+      document.getElementById('Chart'),
+      config
+    );
+  </script>
 
 </body>
+
 </html>
